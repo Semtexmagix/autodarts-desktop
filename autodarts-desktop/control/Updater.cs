15bec695d3d87016eb8d4499d26faae038937031
Luca Bormann <30852569+lbormann@users.noreply.github.com>
2022-10-23 09:26:21 +0200

    Updates version

--- autodarts-desktop/control/Updater.cs
+++ autodarts-desktop/control/Updater.cs
@@ -18,7 +18,7 @@ namespace autodarts_desktop.control
         // ATTRIBUTES
 
         // Increase for new build ..
-        public static readonly string version = "v0.8.9";
+        public static readonly string version = "v0.9.0";
 
         
         public static event EventHandler<ReleaseEventArgs>? NewReleaseFound;
