65d9fbd83c1254839b60f35d74d239a084f21cf5
wusaaa <30852569+lbormann@users.noreply.github.com>
2023-02-28 15:41:20 +0100

    Inc v

--- autodarts-desktop/control/ProfileManager.cs
+++ autodarts-desktop/control/ProfileManager.cs
@@ -383,7 +383,7 @@ namespace autodarts_desktop.control
 
             AppDownloadable autodartsCaller =
                 new(
-                    downloadUrl: "https://github.com/lbormann/autodarts-caller/releases/download/v1.8.2/autodarts-caller.exe",
+                    downloadUrl: "https://github.com/lbormann/autodarts-caller/releases/download/v2.0.1/autodarts-caller.exe",
                     name: "autodarts-caller",
                     helpUrl: "https://github.com/lbormann/autodarts-caller",
                     descriptionShort: "calls out thrown points",
@@ -664,6 +664,10 @@ namespace autodarts_desktop.control
                 }
 
                 autodartsCaller.DownloadUrl = "https://github.com/lbormann/autodarts-caller/releases/download/v2.0.0/autodarts-caller.exe";
+
+                // 37. Mig (Update download version)
+                autodartsCaller.DownloadUrl = "https://github.com/lbormann/autodarts-caller/releases/download/v2.0.1/autodarts-caller.exe";
+
             }
 
             var autodartsExtern = AppsDownloadable.Single(a => a.Name == "autodarts-extern");
